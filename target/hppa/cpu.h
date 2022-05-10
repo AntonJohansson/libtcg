9e8ebd9584626214f42e81857624cccfea93c798
Anton Johansson <anjo@rev.ng>
2022-05-10 12:34:14 +0200

    target: Add `get_tb_mmu_index()` for all frontends
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/hppa/cpu.h
+++ target/hppa/cpu.h
@@ -371,7 +371,19 @@ static inline void cpu_get_tb_cpu_state(CPUHPPAState *env, vaddr *pc,
 
 target_ulong cpu_hppa_get_psw(CPUHPPAState *env);
 void cpu_hppa_put_psw(CPUHPPAState *env, target_ulong);
+target_ureg cpu_hppa_get_psw(CPUHPPAState *env);
+void cpu_hppa_put_psw(CPUHPPAState *env, target_ureg);
 void cpu_hppa_loaded_fr0(CPUHPPAState *env);
+
+static inline int get_tb_mmu_index(uint32_t flags)
+{
+#ifdef CONFIG_USER_ONLY
+    return MMU_USER_INDEX;
+#else
+    uint32_t privilege = (flags >> TB_FLAG_PRIV_SHIFT) & 3;
+    return (flags & PSW_D ? privilege : MMU_PHYS_IDX);
+#endif
+}
 
 #ifdef CONFIG_USER_ONLY
 static inline void cpu_hppa_change_prot_id(CPUHPPAState *env) { }
